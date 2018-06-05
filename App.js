f3842da699c38a0304fc9e6133c18b1a44d3b11a
rhmtrz <2249301106@qq.com>
2018-06-05 10:45:14 +0900

    feat: select-option

--- App.js
+++ App.js
@@ -1,13 +1,61 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Select, Option } from 'react-native-chooser'; 
+
+const data = [
+  {
+    name: "Adania", 
+    age: "25", 
+    position: "leader",
+  }, 
+  {
+    name: "Sonoda", 
+    age: "23", 
+    position: "manager",
+  }, 
+  {
+    name: "Rahmat", 
+    age: "27", 
+    position: "backend engineer",
+  }, 
+]
+
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props); 
+    this.state = {
+      selectUser: data[0],
+    }
+  
+  this.handleSelectUser = (e) => { 
+    const selectUserName = data.filter(d => d.name === e)[0]; 
+
+    this.setState({
+      selectUser: selectUserName, 
+    })
+  }
+      
+  }
   render() {
+    const { selectUser } = this.state;
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text style={styles.titleText}>Hello World</Text>
+        <Select 
+          defaultText = {this.state.name}
+          onSelect={this.handleSelectUser}>
+          {data.map((d, index) => 
+            <Option
+              key={index}
+              value={d.name}
+            >{d.name}</Option>
+          )}
+        </Select>
+        <Text>User Info</Text>
+        <Text>name: {selectUser.name}</Text>
+        <Text>age: {selectUser.age}</Text>
+        <Text>position: {selectUser.position}</Text>
       </View>
     );
   }
@@ -20,4 +68,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  titleText: {
+    color: "blue",
+    fontSize: 30,
+
+  }
 });
